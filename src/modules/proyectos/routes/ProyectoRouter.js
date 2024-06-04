92266458c72e112c29395340921388dd6ca07e63
Brayan Gamboa <brayangt1710@gmail.com>
2024-06-04 13:22:09 -0400

    reportes sin cookies

--- src/modules/proyectos/routes/ProyectoRouter.js
+++ src/modules/proyectos/routes/ProyectoRouter.js
@@ -18,8 +18,8 @@ ProyectoRouter.delete('/eliminar/:id',authRequired, ProyectoController.delete)
 
 //ProyectoRouter.get('/reporte/:id', pdf)
 //ProyectoRouter.get('/Graficaproyecto/:id', ProyectoController.graph)
-ProyectoRouter.get('/:id/pdf',authRequired, ProyectoController.generarPDFProyectoSimple)
-ProyectoRouter.get('/:id/pdf-grafico',authRequired, ProyectoController.generarPDFProyectoGrafico)
-ProyectoRouter.get('/:id/pdf-usuarios',authRequired,  ProyectoController.generarPDFProyectoSimpleUser) 
+ProyectoRouter.get('/:id/pdf', ProyectoController.generarPDFProyectoSimple)
+ProyectoRouter.get('/:id/pdf-grafico', ProyectoController.generarPDFProyectoGrafico)
+ProyectoRouter.get('/:id/pdf-usuarios',  ProyectoController.generarPDFProyectoSimpleUser) 
 
 export default ProyectoRouter
